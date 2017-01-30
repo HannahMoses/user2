98adba15c7c0915e7a00a828642079eb27cab732
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-01-30 15:53:50 -0600

    Code for 2 distinct web pages

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+'''
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
@@ -25,12 +26,63 @@ class MainHandler(webapp2.RequestHandler):
         textarea4 = "<input type ='text' name='email'/>"
 
 
-        message = "All other textareas  will work soon !"
+        message = "All the textareas  will work soon !"
         textarea = "<textarea type='text' style='width:310px'>"+message+"</textarea>"
 
         header = "<h2 style='background-color:rgb(0,180,200);color:white;text-align:center'>USER SIGN-UP</h2>"
         body = "<body style='background-color:rgb(0,180,200)'>Please note that, the first three fields ar required.<br><br></body>"
         submit="<input type='submit'/>"
+        form= ("<form>"+
+
+        textarea1_label+textarea1+"<br><br>"+
+        textarea2_label+textarea2+"<br><br>"+
+        textarea3_label+textarea3+"<br><br>"+
+        textarea4_label+textarea4+"<br><br>"+
+        textarea+"<br><br>"+submit+
+        "</form>")
+        self.response.out.write(header +body+form)
+
+app = webapp2.WSGIApplication([
+    ('/',MainHandler)
+    ],debug=True)
+
+'''
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        textarea1_label ="<label style='font-size:16px;background-color:white;display:inline-block;width:150px'>Username * : </label>"
+        textarea1 = "<input type ='text' name='Username'/>"
+        textarea2_label ="<label style='background-color:white;display:inline-block;width:150px'>Password * : </label>"
+        textarea2 = "<input type ='text' name='password'/>"
+        textarea3_label ="<label style='background-color:white;display:inline-block;width:150px'>Verify password * : </label>"
+        textarea3 = "<input type ='text' name='Username'/>"
+        textarea4_label ="<label style=display:inline-block;width:150px>Email(optional) : </label>"
+        textarea4 = "<input type ='text' name='email'/>"
+
+
+        message = "I still need to validate and display the warnings !"
+        textarea = "<textarea type='text' style='width:500px'>"+message+"</textarea>"
+
+        header = "<h2 style='font-family: 'Times New Roman';color:black' > Signup</h2>"
+        body = "<body style='background-color:white'><br></body>"
+#        header = "<h2 style='background-color:rgb(0,180,200);color:white;text-align:center'>USER SIGN-UP</h2>"
+#        body = "<body style='background-color:rgb(0,180,200)'>Please note that, the first three fields ar required.<br><br></body>"
+
+        submit="<input type='submit' value='Submit Query'/>"
         form= ("<form>"+
 
         textarea1_label+textarea1+"<br><br>"+
