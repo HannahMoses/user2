ecc10e192d691e2232c474866dbb1f45bce0b108
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-01-30 12:08:24 -0600

    4 usernames pink background

--- main.py
+++ main.py
@@ -5,30 +5,38 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 import webapp2
-import cgi
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-#        username_label = "<label style='color:red'>"Username"</label>"
-        username ="Hannah is the username"
-        textarea="<textarea name ='username'>"+username+"</textarea>"
-        form = ("<form style='background-color:pink'method='post'"  +
-                textarea+
+        textarea1_label ="<label style='background-color:pink'>Username : </label>"
+        textarea1 = "<input type ='text' name='Username'/>"
+        textarea2_label ="<label style='background-color:pink'>Password : </label>"
+        textarea2 = "<input type ='text' name='password'/>"
+        textarea2_label ="<label style='background-color:pink'>Username : </label>"
+        textarea2 = "<input type ='text' name='Username'/>"
+        textarea2_label ="<label style='background-color:pink'>Username : </label>"
+        textarea2 = "<input type ='text' name='Username'/>"
 
-               "</form>")
-        content = textarea
-        self.response.write(content)
+        message = "All other textareas  will work soon !"
+        textarea = "<textarea type='number'>"+message+"</textarea>"
+
+        submit="<input type='submit'/>"
+        form= ("<form>"+
+        textarea1_label+textarea1+"<br><br>"+
+        textarea2_label+textarea2+"<br><br>"+
+        textarea2_label+textarea2+"<br><br>"+
+        textarea2_label+textarea2+"<br><br>"+
+        textarea+"<br><br>"+submit+
+        "</form>")
+        self.response.out.write(form)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/',MainHandler)
+    ],debug=True)
